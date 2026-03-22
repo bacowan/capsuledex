0502f3b7d3db7ad09ec7b7c4d0bce9d241be8e01
bacowan <cowan.brendan.a@gmail.com>
2026-03-22 20:20:05 +0900

    removing types since db is no longer typed

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient, SupabaseClient } from "@supabase/supabase-js"
-import { Database } from "../../supabase/database.types"
 
-let supabase: SupabaseClient<Database>
+let supabase: SupabaseClient
 if (!process.env.PROJECT_URL) {
     throw Error("DB_URL environment variable not configured.")
 }
@@ -9,7 +8,7 @@ else if (!process.env.SUPABASE_KEY) {
     throw Error("SUPABASE_KEY environment variable not configured.")
 }
 else {
-    supabase = createClient<Database>(process.env.PROJECT_URL, process.env.SUPABASE_KEY)
+    supabase = createClient(process.env.PROJECT_URL, process.env.SUPABASE_KEY)
 }
 
 export default supabase
